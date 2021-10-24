932ff928f7d40d5b14b45d30457a0d3254cc1e9d
Pavel <82582511+Parohodov@users.noreply.github.com>
2021-10-24 21:45:50 +0300

    Initiate state

---
+++ src/main/java/ru/dataart/academy/java/Calculator.java
@@ -0,0 +1,24 @@
+package ru.dataart.academy.java;
+
+public class Calculator {
+    /**
+     * @param zipFilePath -  path to zip archive with text files
+     * @param character   - character to find
+     * @return - how many times character is in files
+     */
+    public Integer getNumberOfChar(String zipFilePath, char character) {
+        //Task implementation
+        return 0;
+    }
+
+    /**
+     * @param zipFilePath - path to zip archive with text files
+     * @return - max length
+     */
+
+    public Integer getMaxWordLength(String zipFilePath) {
+        //Task implementation
+        return 0;
+    }
+
+}
