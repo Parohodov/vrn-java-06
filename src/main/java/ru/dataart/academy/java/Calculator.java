fc3661bbdfbdfa6c62eac42d18555bee767299c6
Pavel <82582511+Parohodov@users.noreply.github.com>
2021-10-25 16:45:10 +0300

    Error messages updated

--- src/main/java/ru/dataart/academy/java/Calculator.java
+++ src/main/java/ru/dataart/academy/java/Calculator.java
@@ -23,15 +23,17 @@ public class Calculator {
 
         int counter = 0;
 
+
         try (ZipInputStream zin = new ZipInputStream(new BufferedInputStream(new FileInputStream(checkedPath)))) {
             ZipEntry entry;
             while ((entry = zin.getNextEntry()) != null) {
                 if (!entry.isDirectory()) {
-                    counter += countCharsInFile(zin, character);
+                    counter += countCharsInEntry(zin, character);
                 }
                 zin.closeEntry();
             }
         } catch (IOException e) {
+            System.out.format("%s zip file can't be accessed", zipFilePath);
             e.printStackTrace();
         }
         return counter;
@@ -44,6 +46,7 @@ public class Calculator {
     public Integer getMaxWordLength(String zipFilePath) {
         //Task implementation
         int longestWordLen = 0;
+        Character[] separators = new Character[]{' ', '\n'};
 
         String checkedPath = getValidPath(zipFilePath);
         if (checkedPath == null) {
@@ -54,38 +57,17 @@ public class Calculator {
             ZipEntry entry;
             while ((entry = zin.getNextEntry()) != null) { // obtaining next file
                 if (!entry.isDirectory()) {
-//                    longestWordLen = Math.max(longestWordLen, getLongestWordLength(zin));
-                    longestWordLen = Math.max(longestWordLen, getLongestWordLength(zin, new Character[] {' ', '\n'}));
+                    longestWordLen = Math.max(longestWordLen, getLongestWordLength(zin, separators));
                 }
                 zin.closeEntry();
             }
         } catch (IOException e) {
+            System.out.format("%s zip file can't be accessed", zipFilePath);
             e.printStackTrace();
         }
         return longestWordLen;
     }
 
-    /**
-     * @param inStream - path to zip archive with text files
-     * @return - a length of a longest word
-     */
-    private int getLongestWordLength(ZipInputStream inStream) throws IOException {
-        int space = ' ';
-        int eol = '\n';
-        int maxLen = 0;
-        int currLen = 0;
-
-        int readChar;
-        while ((readChar = inStream.read()) != -1) { // reading next file
-            if (readChar == space || readChar == eol) {
-                maxLen = Math.max(maxLen, currLen);
-                currLen = 0;
-            } else {
-                currLen++;
-            }
-        }
-        return maxLen;
-    }
 
     // Returns a longest word length in a file passed within a zip stream
     private int getLongestWordLength(ZipInputStream inStream, Character[] separators) throws IOException {
@@ -111,7 +93,7 @@ public class Calculator {
     }
 
     //
-    private int countCharsInFile(ZipInputStream inStream, char wantedChar) throws IOException {
+    private int countCharsInEntry(ZipInputStream inStream, char wantedChar) throws IOException {
         int readChar;
         int counter = 0;
         while ((readChar = inStream.read()) != -1) {
@@ -135,13 +117,13 @@ public class Calculator {
                 Paths.get(path);
                 checkedPath = path;
             } catch (InvalidPathException pathException) { // The path is invalid
-                System.out.println("Path is invalid");
+                System.out.format("%s path is invalid", path);
                 return null;
             }
         }
 
         if (!Files.exists(Paths.get(checkedPath))) {
-            System.out.println("File doesn't exist");
+            System.out.format("%s file doesn't exist", path);
             return null;
         }
         return checkedPath;
